5375e307be519c6761fec59a8acbb0d4e67c4f3c
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2021-08-07 02:31:12 +0800

    8.7

--- LeetCode/457/main.cpp
+++ LeetCode/457/main.cpp
@@ -1,6 +1,49 @@
 #include <vector>
 using namespace std;
 
+int n;
+
+int next(int s, int i)
+{
+    return ((s % n) + i + n) % n;
+}
+
 bool circularArrayLoop(vector<int> &nums)
 {
+    n = nums.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!nums[i])
+        {
+            continue;
+        }
+
+        int slow = i;
+        int fast = next(nums[i], i);
+
+        while (slow != fast && nums[slow] * nums[fast] > 0 && nums[slow] * nums[next(nums[fast], fast)] > 0)
+        {
+            slow = next(nums[slow], slow);
+            fast = next(nums[fast], fast);
+            fast = next(nums[fast], fast);
+        }
+
+        if (slow == fast && slow != next(nums[slow], slow))
+        {
+            return true;
+        }
+
+        slow = i;
+        fast = next(nums[i], i);
+
+        int pos = i;
+        while (nums[pos] * nums[next(nums[pos], pos)] > 0)
+        {
+            nums[pos] = 0;
+            pos = next(nums[pos], pos);
+        }
+    }
+
+    return false;
 }
