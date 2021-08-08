1dd2690ec344cd8b8dc9b1ce8fe5ea56c38a070d
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2021-08-08 12:56:03 +0800

    8.8

--- LeetCode/847/main.cpp
+++ LeetCode/847/main.cpp
@@ -1,7 +1,40 @@
 #include <vector>
+#include <queue>
 using namespace std;
 
 int shortestPathLength(vector<vector<int>> &graph)
 {
-    
+    int n = graph.size();
+
+    queue<tuple<int, int, int>> q;
+    vector<vector<bool>> vis(n, vector<bool>(1 << n));
+
+    for (int i = 0; i < n; i++)
+    {
+        vis[i][1 << i] = true;
+        q.push({i, 1 << i, 0});
+    }
+
+    while (!q.empty())
+    {
+        auto [cur, state, dist] = q.front();
+        q.pop();
+
+        if (state == (1 << n) - 1)
+        {
+            return dist;
+        }
+
+        for (int next : graph[cur])
+        {
+            int nextState = state | (1 << next);
+            if (!vis[next][nextState])
+            {
+                q.push({next, nextState, dist + 1});
+                vis[next][nextState] = true;
+            }
+        }
+    }
+
+    return 0;
 }
