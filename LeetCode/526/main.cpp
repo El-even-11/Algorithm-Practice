0924988e1a68effdebbe1373e8c4f79add3ecfbc
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2021-08-16 15:55:41 +0800

    8.16

---
+++ LeetCode/526/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int dp[16][2];
+
+int countArrangement(int n)
+{
+    dp[0][0] = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int state = 0; state < 1 << n; state++)
+        {
+            for (int k = 1; k <= n; k++)
+            {
+                if ((state >> (k - 1)) & 1 == 0)
+                {
+                    continue;
+                }
+                if (k % i != 0 && i % k != 0)
+                {
+                    continue;
+                }
+                dp[i][state] += dp[i - 1][state & ~(1 << (k - 1))];
+            }
+        }
+    }
+
+    return dp[n][1 << n - 1];
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << countArrangement(n) << endl;
+
+    return 0;
+}
