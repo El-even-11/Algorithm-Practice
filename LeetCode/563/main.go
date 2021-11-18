24f6048fefe493806d4e81a1632ab61c8fa1a8e4
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2021-11-18 13:07:56 +0800

    11.18

---
+++ LeetCode/563/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "math"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+var ret int
+
+func dfs(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	l := dfs(root.Left)
+	r := dfs(root.Right)
+	if l > r {
+		ret += (l - r)
+	} else {
+		ret += (r - l)
+	}
+	return l + r + root.Val
+}
+
+func findTilt(root *TreeNode) int {
+	dfs(root)
+	return ret
+}
