225e4a7be53a5912826d04c0f0492cc09ade1372
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2021-12-04 00:38:51 +0800

    12.4

---
+++ LeetCode/383/main.go
@@ -0,0 +1,18 @@
+package main
+
+func canConstruct(ransomNote string, magazine string) bool {
+	m := make(map[byte]int)
+
+	for i := 0; i < len(magazine); i++ {
+		m[magazine[i]]++
+	}
+
+	for i := 0; i < len(ransomNote); i++ {
+		if m[ransomNote[i]] == 0 {
+			return false
+		}
+		m[ransomNote[i]]--
+	}
+
+	return true
+}
