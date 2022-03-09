be06758dbe4b094b9157edc53598923d9d48a8ed
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2022-03-09 20:23:55 +0800

    3.9

---
+++ LeetCode/55/main.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    bool canJump(vector<int> &nums)
+    {
+        int farthest = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (i <= farthest)
+            {
+                farthest = max(farthest, i + nums[i]);
+            }
+        }
+        return farthest >= n - 1;
+    }
+};
