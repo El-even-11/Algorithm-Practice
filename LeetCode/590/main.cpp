37e74202ac40ec3161969b5d0a8056f6546e8ba9
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2022-03-12 11:16:06 +0800

    3.12

---
+++ LeetCode/590/main.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution
+{
+public:
+    vector<int> ret;
+
+    vector<int> postorder(Node *root)
+    {
+        dfs(root);
+        return ret;
+    }
+
+    void dfs(Node *root)
+    {
+        if (!root)
+        {
+            return;
+        }
+        for (auto child : root->children)
+        {
+            dfs(child);
+        }
+        ret.push_back(root->val);
+    }
+};
