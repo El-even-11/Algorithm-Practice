1744425dd056630cece706d735863c50c3e6f003
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2022-04-09 11:42:31 +0800

    4.9

---
+++ LeetCode/780/main.go
@@ -0,0 +1,25 @@
+package main
+
+func reachingPoints(sx int, sy int, tx int, ty int) bool {
+	for tx != ty {
+		if sx > tx || sy > ty {
+			return false
+		}
+
+		if sx == tx {
+			return (ty-sy)%sx == 0
+		}
+
+		if sy == ty {
+			return (tx-sx)%sy == 0
+		}
+		
+		if tx > ty {
+			tx = tx - ty
+		} else {
+			ty = ty - tx
+		}
+	}
+
+	return sx == tx && sy == ty
+}
