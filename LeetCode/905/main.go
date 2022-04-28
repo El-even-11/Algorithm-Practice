7241e9d5d51054d0fd63943cd7a7b476927d51e8
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2022-04-28 10:35:44 +0800

    4.28

--- LeetCode/905/main.go
+++ LeetCode/905/main.go
@@ -1,3 +1,25 @@
-func sortArrayByParity(nums []int) []int {
+package main
 
+func sortArrayByParity(nums []int) []int {
+	i := 0
+	j := len(nums) - 1
+
+	for i < j {
+		if nums[i]%2 == 0 {
+			i++
+		}
+		if nums[j]%2 == 1 {
+			j--
+		}
+		if i >= j {
+			break
+		}
+		if nums[i]%2 == 1 && nums[j]%2 == 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
+			j--
+		}
+	}
+
+	return nums
 }
