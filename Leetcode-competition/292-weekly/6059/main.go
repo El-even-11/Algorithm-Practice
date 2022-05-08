d38ec00936678f6bfe05bd5398a6147db43c865a
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2022-05-08 13:16:10 +0800

    5.8

--- Leetcode-competition/292-weekly/6059/main.go
+++ Leetcode-competition/292-weekly/6059/main.go
@@ -5,77 +5,42 @@ package main
 var grid [][]byte
 var m, n int
 
-var exist [101][101][300]int
+var exist [101][101][300]bool
 
 func dfs(i, j int, q int) bool {
-	if exist[i][j][q] > 0 {
-		return exist[i][j][q] == 2
-	}
 
-	if q == 0 {
-		if grid[i][j] == ')' {
-			exist[i][j][q] = 1
-			return false
-		} else {
-			q++
-		}
-	} else {
-
-		if grid[i][j] == ')' {
-			q--
-		} else {
-			q++
-		}
-
-		if q > (m+n)/2 {
-			exist[i][j][q] = 1
-			return false
-		}
+	if q > m+n-i-j-1 {
+		return false
 	}
 
 	if i == m-1 && j == n-1 {
-		if q == 0 {
-			exist[i][j][q] = 2
-		} else {
-			exist[i][j][q] = 1
-		}
 
-		return q == 0
+		return q == 1
 	}
 
-	l := false
-	r := false
-	if i == m-1 {
-		l = dfs(i, j+1, q)
-	} else if j == n-1 {
-		r = dfs(i+1, j, q)
+	if exist[i][j][q] {
+		return false
+	}
+
+	exist[i][j][q] = true
+
+	if grid[i][j] == '(' {
+		q++
 	} else {
-		l = dfs(i, j+1, q)
-		if l {
-			exist[i][j][q] = 2
-			return true	
-		}
-
-		r = dfs(i+1, j, q)
-		if r {
-			exist[i][j][q] = 2
-			return true	
+		q--
+		if q < 0 {
+			return false
 		}
 	}
 
-	if l || r {
-		exist[i][j][q] = 2
-	} else {
-		exist[i][j][q] = 1
-	}
-	return l || r
+	return i < m-1 && dfs(i+1, j, q) || j < n-1 && dfs(i, j+1, q)
 }
 
 func hasValidPath(_grid [][]byte) bool {
 	grid = _grid
 	m = len(grid)
 	n = len(grid[0])
-	if (m+n)%2 == 0 {
+	if (m+n)%2 == 0 || grid[0][0] == ')' || grid[m-1][n-1] == '(' {
 		return false
 	}
 	return dfs(0, 0, 0)
